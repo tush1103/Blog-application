97c9333859010e21db29ac1e451d2cc973b030a8
Tushika <tushikagarg11@gmail.com>
2024-01-26 19:01:19 +0530

    env and appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
+import conf from './conf/conf';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  console.log(conf.appwriteurl);
   return (
     <>
       hi
