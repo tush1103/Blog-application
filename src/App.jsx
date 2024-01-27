e79c5afa108d0c6dc87b99b8d7dcb0b5f74fbdac
Tushika <tushikagarg11@gmail.com>
2024-01-27 13:25:21 +0530

    config redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react'
 import './App.css'
-import conf from './conf/conf';
+import authService from './appwrite/auth'
+import { useDispatch } from 'react-redux'
+import { login, logout } from './store/authSlice'
+import Header from './components/Header/HEADER.JSX'
+import Footer from './components/Footer/Footer'
 
-function App() {
-  console.log(conf.appwriteurl);
-  return (
-    <>
-      hi
-    </>
-  )
+function App () {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService
+      .getCurrentUSer()
+      .then(userData => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(setLoading(false))
+  }, [])
+  return !loading ? <div className='min-h-screen flex flex-wrap context-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        {/* <Outlet/> */}
+      </main>
+      <Footer/>
+    </div>
+  </div> : null
 }
 
 export default App
