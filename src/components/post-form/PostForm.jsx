c0a3e1be9921ff16dd1d769af963b1f8692df9c8
Tushika <tushikagarg11@gmail.com>
2024-01-28 13:07:58 +0530

    postform

---
+++ src/components/post-form/PostForm.jsx
@@ -0,0 +1,142 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useCallback, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { Button, Input, Select, RTE } from '../index'
+import appwriteService from '../../appwrite/config'
+import { useNavigate } from 'react-router-dom'
+import { UseSelector, useSelector } from 'react-redux'
+
+function PostForm ({ post }) {
+  const { register, handleSubmit, watch, setValue, control, getValues } =
+    useForm({
+      defaultValues: {
+        title: post?.title || '',
+        slug: post?.slug || '',
+        content: post?.content || '',
+        status: post?.status || 'active'
+      }
+    })
+
+  const navigate = useNavigate()
+  const userData = useSelector(state => state.auth.userData)
+
+  const submit = async data => {
+    if (post) {
+      const file = data.image[0]
+        ? appwriteService.fileUpload(data.image[0])
+        : null
+      //deleting old picture
+      if (file) {
+        appwriteService.fileDelete(post.featuredImage)
+      }
+      //updating our post with new picture
+      const dbPost = await appwriteService.updatePost(post.$id, {
+        ...data,
+        featuredImage: file ? file.$id : undefined
+      })
+      if (dbPost) {
+        navigate(`/post/${dbPost.$id}`)
+      }
+    }
+
+    //if you dont have the post
+    else {
+      const file = await appwriteService.fileUpload(data.image[0])
+
+      if (file) {
+        const fileId = file.$id
+        data.featuredImage = fileId
+        const dbPost = await appwriteService.createPost({
+          ...data,
+          userId: userData.$id
+        })
+        if (dbPost) {
+          navigate(`/post/${dbPost.$id}`)
+        }
+      }
+    }
+  }
+
+  const slugTransform = useCallback(value => {
+    if (value && typeof value === 'string') {
+      return value
+        .trim()
+        .toLowerCase()
+        .replace(/^[a-zA-Z\d]+/g, '-')
+    }
+    return ''
+  }, [])
+
+  useEffect(() => {
+    const subscribtion = watch((value, { name }) => {
+      if (name === 'title') {
+        setValue('slug', slugTransform(value.title, { shouldValidate: true }))
+      }
+    })
+    return () => {
+      subscribtion.unsubscribe()
+    }
+  }, [watch, slugTransform, setValue])
+  return (
+    <form onSubmit={handleSubmit(submit)} className='flex flex-wrap'>
+      {' '}
+      <div className='w-2/3 px-2'>
+        <Input
+          label='Title :'
+          placeholder='Title'
+          className='mb-4'
+          {...register('title', { required: true })}
+        />
+        <Input
+          label='Slug :'
+          placeholder='Slug'
+          className='mb-4'
+          {...register('slug', { required: true })}
+          onInput={e => {
+            setValue('slug', slugTransform(e.currentTarget.value), {
+              shouldValidate: true
+            })
+          }}
+        />
+        <RTE
+          label='Content :'
+          name='content'
+          control={control}
+          defaultValue={getValues('content')}
+        />
+      </div>
+      <div className='w-1/3 px-2'>
+        <Input
+          label='Featured Image :'
+          type='file'
+          className='mb-4'
+          accept='image/png, image/jpg, image/jpeg, image/gif'
+          {...register('image', { required: !post })}
+        />
+        {post && (
+          <div className='w-full mb-4'>
+            <img
+              src={appwriteService.getFilePreview(post.featuredImage)}
+              alt={post.title}
+              className='rounded-lg'
+            />
+          </div>
+        )}
+        <Select
+          options={['active', 'inactive']}
+          label='Status'
+          className='mb-4'
+          {...register('status', { required: true })}
+        />
+        <Button
+          type='submit'
+          bgColor={post ? 'bg-green-500' : undefined}
+          className='w-full'
+        >
+          {post ? 'Update' : 'Submit'}
+        </Button>
+      </div>
+    </form>
+  )
+}
